e19b8797f5da993e0f7cb1936440ff6edcca4203
Kacper Kędzierski <46610139+Nsquik@users.noreply.github.com>
2023-05-02 23:19:13 +0200

    Initial Commit

---
+++ breaking-bad-client/breaking_bad_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  breaking_bad_clientApp.swift
+//  breaking-bad-client
+//
+//  Created by Kacper Kędzierski on 02/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct breaking_bad_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
